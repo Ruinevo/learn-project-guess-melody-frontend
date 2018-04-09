4285e4942ab3d7bef1de1319cffd0cae1bd064d8
Rinat Sadykov <sadykovra@mail.ru>
2018-04-09 22:29:30 +0500

    Задание #6. Исправил замечания v.1

--- js/data/game-data.js
+++ js/data/game-data.js
@@ -1,55 +1,57 @@
-const COUNTING_RULES = {
-  pointsForSuccessAnswer: 1,
-  pointsForUnsuccessAnswer: -2,
-  pointsForFastAndSuccessAnswer: 2,
-  timeOfFastAnswer: 30
+const SCORES = {
+  rightAnswer: 1,
+  wrongAnswer: -2,
+  quickAnswer: 2
 };
 
-const countsPointsForAnswer = (elem) => {
-  if (elem.success && elem.time > COUNTING_RULES.timeOfFastAnswer) {
-    return COUNTING_RULES.pointsForSuccessAnswer;
-  } else if (elem.success && elem.time < COUNTING_RULES.timeOfFastAnswer) {
-    return COUNTING_RULES.pointsForFastAndSuccessAnswer;
-  } else if (!elem.success) {
-    return COUNTING_RULES.pointsForUnsuccessAnswer;
+const QUICK_ANSWER_TIME_LIMIT = 30; // s
+const ROUNDS = 10;
+
+const countsPointsForAnswer = (answer) => {
+  if (answer.success && answer.time > QUICK_ANSWER_TIME_LIMIT) {
+    return SCORES.rightAnswer;
+  } else if (answer.success && answer.time < QUICK_ANSWER_TIME_LIMIT) {
+    return SCORES.quickAnswer;
   }
+  return SCORES.wrongAnswer;
 };
 
-export const countsPointsForGame = (answers, lives) => {
-  if (answers.length < 10 || lives <= 0) {
+export const countsPointsForGame = (answers, lives) => { // export
+  if (answers.length < ROUNDS || lives <= 0) {
     return -1;
   }
-  const calculatedAnswers = answers.reduce((sum, current) => {
+  const calculatedScores = answers.reduce((sum, current) => {
     return sum + countsPointsForAnswer(current);
   }, 0);
-  return calculatedAnswers;
+  return calculatedScores;
 };
 
-export const showResult = (resultsOfOtherPlayers, resultOfCurrentPlayer) => {
+export const showResult = (resultsOfOtherPlayers, resultOfCurrentPlayer) => { // export
   if (resultOfCurrentPlayer.lives <= 0) {
     return `У вас закончились все попытки. Ничего, повезёт в следующий раз!`;
   } else if (resultOfCurrentPlayer.time <= 0) {
     return `Время вышло! Вы не успели отгадать все мелодии`;
   } else {
-    resultsOfOtherPlayers.push(resultOfCurrentPlayer.points);
-    resultsOfOtherPlayers.sort((a, b) => {
-      return b - a;
-    });
-    const positionOfCurrentPlayer = resultsOfOtherPlayers.indexOf(resultOfCurrentPlayer.points) + 1;
-    const percent = (resultsOfOtherPlayers.length - positionOfCurrentPlayer) * 100 / resultsOfOtherPlayers.length;
-    return `Вы заняли ${positionOfCurrentPlayer} место из ${resultsOfOtherPlayers.length} игроков. Это лучше, чем у ${percent}% игроков`;
+    const dirtyResultsOfOtherPlayers = resultsOfOtherPlayers.slice();
+    dirtyResultsOfOtherPlayers.push(resultOfCurrentPlayer.points);
+    dirtyResultsOfOtherPlayers.sort((a, b) => b - a);
+    const positionOfCurrentPlayer = dirtyResultsOfOtherPlayers.indexOf(resultOfCurrentPlayer.points) + 1;
+    const percent = (dirtyResultsOfOtherPlayers.length - positionOfCurrentPlayer) * 100 / dirtyResultsOfOtherPlayers.length;
+    return `Вы заняли ${positionOfCurrentPlayer} место из ${dirtyResultsOfOtherPlayers.length} игроков. Это лучше, чем у ${percent}% игроков`;
   }
 };
 
-export const createTimer = (fulltime) => {
+export const createTimer = (currentTime) => { // export
   const timer = {
-    time: fulltime,
-    tick: function () { // не использую стрелочную функцию из за this
-      if (this.time > 0) {
-        this.time--;
-      } else {
-        return `Время вышло`;
+    tick: () => {
+      if (typeof currentTime !== `number`) {
+        throw new Error(`currentTime should be of type number`);
       }
+      if (currentTime > 0) {
+        currentTime--;
+        return currentTime;
+      }
+      return `Время вышло`;
     }
   };
   return timer;
--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -18,6 +18,14 @@ describe(`Calculates points of player`, () => {
   it(`should return 20, if player answered all questions correctly and fast`, () => {
     assert.equal(20, countsPointsForGame([{success: true, time: 20}, {success: true, time: 20}, {success: true, time: 20}, {success: true, time: 20}, {success: true, time: 20}, {success: true, time: 20}, {success: true, time: 20}, {success: true, time: 20}, {success: true, time: 20}, {success: true, time: 20}]));
   });
+
+  it(`Should not return -1, if the player answered with different speed`, () => {
+    assert.notEqual(-1, countsPointsForGame([{success: true, time: 25}, {success: true, time: 30}, {success: true, time: 35}, {success: true, time: 40}, {success: true, time: 30}, {success: true, time: 20}, {success: true, time: 120}, {success: true, time: 220}, {success: true, time: 200}, {success: true, time: 210}]));
+  });
+
+  it(`Should calculate the scores if the player answered with different speed, but correctly`, () => {
+    assert.equal(6, countsPointsForGame([{success: true, time: 25}, {success: true, time: 30}, {success: true, time: 35}, {success: true, time: 40}, {success: true, time: 30}, {success: true, time: 20}, {success: true, time: 120}, {success: true, time: 220}, {success: true, time: 200}, {success: true, time: 210}]));
+  });
 });
 
 describe(`Display results`, () => {
@@ -30,15 +38,46 @@ describe(`Display results`, () => {
     assert.equal(`Время вышло! Вы не успели отгадать все мелодии`, showResult([4, 8, 12, 16], {time: 0}));
   });
 
-  it(`Вы заняли 2 место из 5 игроков. Это лучше, чем у 60% игроков`, () => {
+  it(`should return "Вы заняли 2 место из 5 игроков. Это лучше, чем у 60% игроков" if the player got the second place`, () => {
     assert.equal(`Вы заняли 2 место из 5 игроков. Это лучше, чем у 60% игроков`, showResult([4, 8, 12, 16], {points: 14, time: 120, lives: 3}));
   });
+
+  it(`should return "Вы заняли 1 место из 5 игроков. Это лучше, чем у 80% игроков" if the player got the first place`, () => {
+    assert.equal(`Вы заняли 1 место из 5 игроков. Это лучше, чем у 80% игроков`, showResult([1, 2, 3, 4], {points: 14, time: 120, lives: 3}));
+  });
+
+  it(`should return "Вы заняли 10 место из 10 игроков. Это лучше, чем у 0% игроков" if the player got the last place `, () => {
+    assert.equal(`Вы заняли 10 место из 10 игроков. Это лучше, чем у 0% игроков`, showResult([24, 25, 26, 27, 28, 29, 30, 31, 31], {points: 14, time: 120, lives: 3}));
+  });
+
+  it(`should return "Вы заняли 8 место из 11 игроков. Это лучше, чем у 27.272727272727273% игроков" if the percent is not an integer`, () => {
+    assert.equal(`Вы заняли 8 место из 11 игроков. Это лучше, чем у 27.272727272727273% игроков`, showResult([24, 25, 26, 27, 28, 29, 30, 12, 13, 0], {points: 14, time: 120, lives: 3}));
+  });
+
+  it(`should return "Вы заняли 1 место из 1 игроков. Это лучше, чем у 0% игроков" if only one result in array of results`, () => {
+    assert.equal(`Вы заняли 1 место из 1 игроков. Это лучше, чем у 0% игроков`, showResult([], {points: 14, time: 120, lives: 3}));
+  });
+
+
 });
 
 describe(`Creating timer`, () => {
 
   it(`should return "Время вышло", if time is up`, () => {
     assert.equal(`Время вышло`, createTimer(0).tick());
+    assert.equal(`Время вышло`, createTimer(-100).tick());
+  });
+
+  it(`should return time reduced by one`, () => {
+    assert.equal(499, createTimer(500).tick());
+  });
+
+  it(`should not allow set without 'currentTime' and non number value`, () => {
+    assert.throws(() => createTimer().tick(), /currentTime should be of type number/);
+    assert.throws(() => createTimer(`string`).tick(), /currentTime should be of type number/);
+    assert.throws(() => createTimer([]).tick(), /currentTime should be of type number/);
+    assert.throws(() => createTimer({}).tick(), /currentTime should be of type number/);
   });
 
 });
+
